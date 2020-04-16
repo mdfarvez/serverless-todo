18e4260db6c51b1ea62d153d72e542bfc24c031c
MD FARVEZ <mdfarvez49@gmail.com>
2020-04-16 21:52:19 +0200

    Adding restriction for get todo item
    Adding restriction for get todo item. So user will see
    only his own todo item not others people

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,16 +1,26 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 import 'source-map-support/register'
 import * as AWS from "aws-sdk"
+import { getUserId } from '../utils'
 
 const todosTable = process.env.TODOS_TABLE
+const userIndex = process.env.TODO_INDEX
 const docClient = new AWS.DynamoDB.DocumentClient()
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   console.log('Processing Events', JSON.stringify(event))
 
+  const userId = getUserId(event)
+
   const result = await docClient
-    .scan({
-      TableName: todosTable
+    .query({
+      TableName: todosTable,
+      IndexName: userIndex,
+      KeyConditionExpression: 'userId = :id',
+      ExpressionAttributeValues: {
+        ':id': userId
+      },
+      ScanIndexForward: false
     })
     .promise()
 
