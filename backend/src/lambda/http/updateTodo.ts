fc01a6df3a3b75b8f57cad55296f94bca2abc9f3
MD FARVEZ <mdfarvez49@gmail.com>
2020-04-16 23:24:17 +0200

    Update todo items
    Added update options for serverless todo App

--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -1,13 +1,40 @@
-import 'source-map-support/register'
-
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import 'source-map-support/register'
+import * as AWS from "aws-sdk"
 
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+
+const todosTable = process.env.TODOS_TABLE
+const docClient = new AWS.DynamoDB.DocumentClient()
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
   const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
-  console.log(`${todoId} ${updatedTodo}`)
-  // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-  return undefined
+
+  await docClient
+    .update({
+      TableName: todosTable,
+      Key: {
+        "todoId": todoId
+      },
+      UpdateExpression: "set #name=:n, dueDate=:dD, done=:d",
+      ExpressionAttributeValues: {
+        ":n": updatedTodo.name,
+        ":dD": updatedTodo.dueDate,
+        ":d": updatedTodo.done
+      },
+      ExpressionAttributeNames: {
+        "#name": "name"
+      },
+      ReturnValues: "UPDATED_NEW"
+    })
+    .promise()
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: 'Updated Successfully'
+  }
 }
