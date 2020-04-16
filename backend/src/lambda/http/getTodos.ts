3732fad09113f3a4268b437ba07a65825d324139
MD FARVEZ <mdfarvez49@gmail.com>
2020-04-16 19:01:27 +0200

    Added get Todo item options
    Added get todos option to our serverless app and added
    dynamodb for todoapp

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,9 +1,26 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 import 'source-map-support/register'
+import * as AWS from "aws-sdk"
 
+const todosTable = process.env.TODOS_TABLE
+const docClient = new AWS.DynamoDB.DocumentClient()
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    
+  console.log('Processing Events', JSON.stringify(event))
 
-  return undefined
+  const result = await docClient
+    .scan({
+      TableName: todosTable
+    })
+    .promise()
+
+  const items = result.Items
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({ items })
+  }
 }
