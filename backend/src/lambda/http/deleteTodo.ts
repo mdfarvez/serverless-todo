cd3ee1d5278ce8b7b99654f792817c30a3ac2ae8
MD FARVEZ <mdfarvez49@gmail.com>
2020-04-16 23:36:51 +0200

    Adding Delete option
    Adding delete options to serverless todo App

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -1,10 +1,29 @@
-import 'source-map-support/register'
-
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import 'source-map-support/register'
+import * as AWS from "aws-sdk"
+
+const todosTable = process.env.TODOS_TABLE
+const docClient = new AWS.DynamoDB.DocumentClient()
+
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  console.log('Process delete event', JSON.stringify(event))
   const todoId = event.pathParameters.todoId
-  console.log(todoId)
-  // TODO: Remove a TODO item by id
-  return undefined
+
+  await docClient
+    .delete({
+      TableName: todosTable,
+      Key: {
+        "todoId": todoId
+      }
+    })
+    .promise()
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: "Deleted successfully!"
+  }
 }
