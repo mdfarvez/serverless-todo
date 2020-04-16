7df688304798c0ab3c0c4a0e12e29431c1b38d65
MD FARVEZ <mdfarvez49@gmail.com>
2020-04-16 16:01:16 +0200

    Adding Auth0 verification for backend
    Adding Auth0 verification using RS256 algorithm
    to backend service so we can verify user logged with
    3rd party login

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,7 +1,9 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 import 'source-map-support/register'
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  // TODO: Get all TODO items for a current user
+    
+
+  return undefined
 }
