5201e46e88f4a7da744551824441f96a69a7ed95
MD FARVEZ <mdfarvez49@gmail.com>
2020-04-18 13:00:35 +0200

    Removing configuration for Client side

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,8 +1,8 @@
-const apiId = 'pqzcznluj4'
+const apiId = '****' // Auth0 appId
 export const apiEndpoint = `https://${apiId}.execute-api.eu-central-1.amazonaws.com/dev`
 
 export const authConfig = {
-  domain: 'mdfarvez.eu.auth0.com',            // Auth0 domain
-  clientId: 'xuOrSHbtHtMwjyToLo0O4WMbCgTP2Nlc',          // Auth0 client id
+  domain: '***',            // Auth0 domain
+  clientId: '***',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
